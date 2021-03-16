1d8a43992f903263e05384484436ce3a368d63e4
josegomez13 <jose.gomez13@udea.edu.co>
2021-03-16 09:00:25 -0500

    Añadir cambios esteticos

---
+++ Lab1_1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int NumA, NumB = 0;
+    cout << "Digite numero A:" << endl;
+    cin >> NumA;
+    cout << "Digite numero B:" << endl;
+    cin >> NumB;
+    cout << "La division es: " << NumA/NumB << endl;
+    return 0;
+}
